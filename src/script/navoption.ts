c1073528c1ec6d8d1829ef4a3a2241e0a7182631
Gyorgy Rethy <rethygy0410@gmail.com>
2019-08-15 10:55:39 +0100

    Add more animations to nav options.

--- src/script/navoption.ts
+++ src/script/navoption.ts
@@ -1,16 +1,34 @@
 // @ts-ignore: It does not recognize this as a module even tho it is (amd)
 import template = require('handlebars/navoption.hbs.js');
-import {UiComponent} from "./ui-component";
+import {eventHandler, UiComponent} from "./ui-component";
+
+const optionsContainerSelector: string = '#options-container';
 
 export class NavOption extends UiComponent {
+    private prevHeight: number;
+
     constructor(containerGen: () => HTMLElement, state: any) {
         super(containerGen, template, state);
 
+        this.prevHeight = 0;
+
         this.render();
+    }
+
+    setState(newState: INavOptionState | null) {
+        if (!newState) newState = {prevHeight: 0, navOptions: []};
+        newState.prevHeight = this.prevHeight;
+        super.setState(newState);
+    }
+
+    @eventHandler(optionsContainerSelector, 'animationend')
+    private saveHeight(e: Event) {
+        this.prevHeight = this.getElement(optionsContainerSelector).offsetHeight;
     }
 }
 
 export interface INavOptionState {
+    prevHeight: number;
     navOptions: ISingleNavOption[];
 }
 
